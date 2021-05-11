d241a4ffd523a01ba6c39ae393f13c580700cbaa
JaiNiranjanM <jai.niranjanm@gmail.com>
2021-05-11 19:59:25 +0530

    Update WeatherApp.test.js

--- src/WeatherApp.test.js
+++ src/WeatherApp.test.js
@@ -1,8 +1,39 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import Adapter from "enzyme-adapter-react-16";
+import { shallow, configure } from "enzyme";
+import WeatherApp from "./WeatherApp";
+import WeatherData from "./mockData";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+configure({ adapter: new Adapter() });
+
+describe("WeatherApp Component", () => {
+  let props;
+  let wrapper;
+  const currentWeatherData = WeatherData.weatherData;
+  const weatherForecastData = WeatherData.forecastData;
+  beforeEach(() => {
+    const spy = jest.spyOn(React, "useEffect").mockImplementation((f) => f());
+
+    props = {
+      fetchCurrentWeatherData: jest.fn().mockResolvedValue(currentWeatherData),
+      fetchWeatherForecastData: jest
+        .fn()
+        .mockResolvedValue(weatherForecastData),
+    };
+
+    wrapper = shallow(<WeatherApp {...props} />);
+  });
+
+  it("should render WeatherApp component", () => {
+    const component = shallow(<WeatherApp />);
+    expect(component.getElements()).toMatchSnapshot();
+  });
+
+  it("loads the CurrentWeatherData", () => {
+    expect(props.fetchCurrentWeatherData).toBeCalledTimes(0);
+  });
+
+  it("loads the WeatherForecastData", () => {
+    expect(props.fetchWeatherForecastData).toBeCalledTimes(0);
+  });
 });
